e8a87bab5f915e07b2b91fed4194ffcad6190a6f
SOFT356-Genshushu <zhuang.ma@students.plymouth.ac.uk>
2020-02-07 21:43:27 +0000

    init Project

---
+++ src/main/java/com/Mz/Comprehensive_Community/ComprehensiveCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.Mz.Comprehensive_Community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComprehensiveCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComprehensiveCommunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Mz/Comprehensive_Community/ComprehensiveCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Mz.Comprehensive_Community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComprehensiveCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
